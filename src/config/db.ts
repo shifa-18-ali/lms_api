17b704e0f92b5b0d2cd936cee8647ba5036ec550
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-02 19:57:36 +0530

    connected to db atlas

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
  import mongoose from "mongoose";
- const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/lms";
+ const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://shifa_db_ali12:shifa_18ali@cluster0.hegt8xi.mongodb.net/lms";
 
 export const connectDb=async ()=>{
     
