7bb34182e17bbf2f53ecaeaa3b028151744f5194
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-02 22:08:59 +0530

    mongodb as string

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
  import mongoose from "mongoose";
- const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://shifa_db_ali12:shifa_18ali@cluster0.hegt8xi.mongodb.net/lms";
+ const MONGO_URI = process.env.MONGO_URI as string|| "mongodb+srv://shifa_db_ali12:shifa_18ali@cluster0.hegt8xi.mongodb.net/lms";
 
-export const connectDb=async ()=>{
+export const connectDb=async () : Promise<void> =>{
     
         mongoose.connect( MONGO_URI)
 .then(()=>console.log('db connected')) 
