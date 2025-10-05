e40392d604dfb1c68c5c1a117fc08a1b82f7a25a
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-05 19:55:14 +0530

    teacher table update

--- src/Routes/teacherRoutes.ts
+++ src/Routes/teacherRoutes.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/createTeacher", createTeacher);
 router.get("/getTeacher", getTeachers);
 router.get("/getTeacherbyid/:id", getTeacherById);
-router.put("/upTeacher:id", updateTeacher);
+router.put("/upTeacher/:id", updateTeacher);
 router.delete("/delTeacher/:id", deleteTeacher);
 
 export default router;
