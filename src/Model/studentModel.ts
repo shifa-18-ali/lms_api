f883750d90bb2cf18683ea1e2162210c1b97bcb8
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-11 22:04:44 +0530

    update student model

--- src/Model/studentModel.ts
+++ src/Model/studentModel.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const StudentSchema: Schema = new Schema(
   {
     name: { type: String, required: true },
-    gender: { type: String,required: true }, 
+  
    
   
   },
