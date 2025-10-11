2cdd945af1202124b4c683b166b0e6888a32ced4
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-11 21:51:42 +0530

    update teacher

--- src/Model/teacherModel.ts
+++ src/Model/teacherModel.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 const TeacherSchema=new mongoose.Schema({
     name: { type: String, required: true },
     dob:{type: Date, required: true},
-    gender:{ type: String, required: true },
+    
     phoneNo:{ type: Number, required: true },
     address:{ type: String, required: true },
     bio: { type: String },
