3c64b058b5520e680bf3a16b5e6595bc6f3e147e
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-13 10:38:33 +0530

    updated table

--- src/Model/teacherModel.ts
+++ src/Model/teacherModel.ts
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose'
 const TeacherSchema=new mongoose.Schema({
     name: { type: String, required: true },
-    dob:{type: Date, required: true},
+    experience:{ type: String, required: true },
+    subject:{ type: String, required: true },
+    // dob:{type: Date, required: true},
     
-    phoneNo:{ type: Number, required: true },
-    address:{ type: String, required: true },
-    bio: { type: String },
+    // phoneNo:{ type: Number, required: true },
+    // address:{ type: String, required: true },
+    // bio: { type: String },
     qualification: { type: String },
-    specialization: { type: String },
-    assigned_courses: [{ type: String }],
-     profile_picture: {type:String,required:true}
+    // specialization: { type: String },
+    // assigned_courses: [{ type: String }],
+    //  profile_picture: {type:String,required:true}
   },
   { timestamps: true 
 
