2d9933084b2b7b3c87812fcf61f742d361fb76c0
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-13 10:46:36 +0530

    changes role letter

--- src/Model/userModel.ts
+++ src/Model/userModel.ts
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["teacher", "student"], required: true },
+  role: { type: String, enum: ["Teacher", "Student"], required: true },
   profileId: { type: mongoose.Schema.Types.ObjectId, refPath: "role" } 
   },
   { timestamps: true }
