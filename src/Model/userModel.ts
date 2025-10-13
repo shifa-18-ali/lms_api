fed388085f2c891fe1730a778c41c37c3e9c5a64
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-13 10:18:34 +0530

    update

--- src/Model/userModel.ts
+++ src/Model/userModel.ts
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     name: { type: String, required: true },
     password: { type: String, required: true },
  email: { type: String, required: true, unique: true },
- gender:{ type: String, required: true, unique: true },
+ gender:{ type: String, required: true},
    role: { type: String, enum: ["teacher", "student","admin"], required: true },
     
     dob:{type: Date, required: true},
