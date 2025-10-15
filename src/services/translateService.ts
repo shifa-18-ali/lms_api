5d35ec50df1d2d667996350a9c801d41f784ab4d
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-15 21:16:09 +0530

    updated url services

--- src/services/translateService.ts
+++ src/services/translateService.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const translateText = async (text: string, targetLang: string) => {
   try {
     const response = await axios.post(
-      "https://libretranslate.com/languages",
+      "https://translate.argosopentech.com/translate",
       {
         q: text,
         source: "auto",
