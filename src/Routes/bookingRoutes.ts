7db0654721d9c0db2e9951673e17901a473112c4
shifa-18-ali <shifaalidhn@gmail.com>
2025-10-18 15:28:23 +0530

    added new route of booking and updated controller

--- src/Routes/bookingRoutes.ts
+++ src/Routes/bookingRoutes.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import {
   createBooking,
-  getAllBookings,
+  getBookingDetails,
   getBookingsByEmail
 } from "../controllers/bookingController";
 
 const router = express.Router();
 
 router.post("/createBooking", createBooking);
-router.get("/getAllBookings", getAllBookings);
+router.get("/getBookingDetails", getBookingDetails);
 router.get("/getBookinguser/:email", getBookingsByEmail);
 
 export default router;
