db6ae4e1921bcf140ba9f11329d251d70dbe1ff2
shifa-18-ali <shifaalidhn@gmail.com>
2026-05-11 20:59:14 +0530

    updated teacher getassignedcoursebyteacher

--- src/controllers/teacher.controller.ts
+++ src/controllers/teacher.controller.ts
@@ -219,9 +219,9 @@ export const getAssignedCourseByTeacher = async (
   res: Response
 ) => {
   try {
-    const _id = req.params;
+    const _id = req.params.id;
 
-    const teacher = await Teacher.findOne({userId: _id })
+    const teacher = await Teacher.findById( _id )
       .populate("assigned_courseid", "courseTitle");
 
     if (!teacher) {
